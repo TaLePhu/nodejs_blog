64f7ba3340dc3d547c70c2e6ec983565c6a470be
TaLePhu <lephu18062@gmail.com>
2025-03-24 20:32:14 +0700

    form default behavior

--- src/index.js
+++ src/index.js
@@ -4,25 +4,25 @@ import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);// lấy URL của file hiện tại, chuyển đổi URL thành đường dẫn thực tế
+const __dirname = path.dirname(__filename);// lấy thư mục chứa file hiện tại
 
 const app = express();
 const port = 3000;
 
 //link static file
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));// vd: Nếu có file public/style.css, thì có thể truy cập qua đường dẫn http://localhost:3000/style.css
 
 
-//log http
-app.use(morgan('combined'));
+//log request HTTP
+// app.use(morgan('combined'));
 
 // Template engine
-app.engine('.hbs', engine({
+app.engine('.hbs', engine({   //Khai báo express-handlebars làm template engine với phần mở rộng .hbs
   extname: '.hbs'
 }));
-app.set('view engine', '.hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set('view engine', '.hbs'); //Xác định Handlebars làm view engine mặc định.
+app.set('views', path.join(__dirname, 'resources/views'));// Đặt thư mục chứa file giao diện (.hbs).
 
 console.log('Path: ', path.join(__dirname, 'resources/views'));
 
@@ -33,6 +33,12 @@ app.get('/', (req, res) => {
 app.get('/news', (req, res) => {
   res.render('news');
 });
+
+app.get('/search', (req, res) => {
+  res.render('search');
+});
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
