2c3bf0c33f1cf93456392d399e10b946a1d7f04a
TaLePhu <lephu18062@gmail.com>
2025-03-24 14:38:16 +0700

    express handlebars

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import { engine } from 'express-handlebars';
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+
+// Template engine
+app.engine('.hbs', engine({
+  extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+console.log('Path: ', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/news', (req, res) => {
+  res.render('news');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
